6401c5bfd5e46ede5c3389e47712fda1935abc34
Orcutt <Jimmy.Orcutt@energyTransfer.com>
2017-06-07 15:15:51 -0500

    Crash screen

--- pygameTest.py
+++ pygameTest.py
@@ -31,23 +31,41 @@ carImg = pygame.image.load('racecar.png')
 def car(x,y):
     gameDisplay.blit(carImg,(x,y))
 
+def text_objects(text,font):
+    textSurface = font.redner(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freeansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text,largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+def crash():
+    message_display('You Crashed')
+
 def game_loop():
     # Starting points for car defined
     x =  (display_width * 0.45)
     y = (display_height * 0.8)
 
-    # 
     x_change = 0
     
     gameExit = False
 
-    while not crashed:
+    while not gameExit:
         # Game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                crashed = True
+                gameExit = True
         
-            #############################
+            # A key goes left D key goes right
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
                     x_change = -5
@@ -67,6 +85,9 @@ def game_loop():
         # Paint the game surface white.    
         gameDisplay.fill(white)
         car(x,y)
+
+        if x > display_width - car_width or x < 0:
+            crash()
         
         # Updates a portion of the game surface. Without arguments is the same as display.flip().
         # Whole surface is updated
@@ -75,5 +96,6 @@ def game_loop():
         # Frames per second
         clock.tick(60)
 
+game_loop()
 pygame.quit()
 quit()
